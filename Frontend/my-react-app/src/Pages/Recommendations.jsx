117728c47d4e444eaa68bfe1830a703001aa7b2b
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-07-07 23:33:27 +0545

    Feat: Added Recommendation Component

---
+++ Frontend/my-react-app/src/Pages/Recommendations.jsx
@@ -0,0 +1,13 @@
+import ImageUpload from "../Components/ImageUpload";
+import Navbar from "../Components/ui/Navbar";
+import Footer from "../Components/ui/Footer";
+const Recommendation = () => {
+  return (
+    <div>
+      <Navbar />
+      <ImageUpload />
+      <Footer />
+    </div>
+  );
+};
+export default Recommendation;
